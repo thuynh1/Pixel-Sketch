e0ee4c0d86f9a353c3e2309a09ca19052d30d460
Tony Huynh <tonyduy092@gmail.com>
2020-11-18 08:33:53 -0800

    Add canvas drawing interface using mouse

--- index.js
+++ index.js
@@ -1,34 +1,75 @@
+// GLOBALS
+let isDrawing = false;
+
+/**
+ * Pixel
+ */
 function createPixel() {
     const pixel = document.createElement("div");
     pixel.classList.add("pixel");
+    pixel.addEventListener("mousedown", pixelMouseDown);
+    pixel.addEventListener("mouseover", pixelMouseOver);
+    pixel.addEventListener("mouseup", pixelMouseUp);
     return pixel;
 }
 
-// TODO: Should we have 2 sliders to change the height and width?
-function createScreen(size=16) {
-    clearScreen();
-    screen.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    screen.style.gridTemplateRows = `repeate(${size}, 1fr)`;
+function pixelMouseDown(event) {
+    event.target.classList.add("pixel--set");
+    isDrawing = true;
+}
+
+function pixelMouseOver(event) {
+    if (isDrawing === true &&
+        event.target.classList[0] === "pixel") {
+        event.target.classList.add("pixel--set");
+    }
+}
+
+function pixelMouseUp(event) {
+    if (isDrawing === true) {
+        event.target.classList.add("pixel--set");
+        isDrawing = false;
+    }
+}
+
+/**
+ * Canvas 
+ */
+function createCanvas(size=16) {
+    clearCanvas();
+    canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    canvas.style.gridTemplateRows = `repeate(${size}, 1fr)`;
     for (let h = 0; h < size; h += 1) {
         for (let w = 0; w < size; w += 1) {
             let pixel = createPixel();
-            screen.appendChild(pixel);
+            canvas.appendChild(pixel);
         }
     }
 }
 
-function clearScreen() {
-    while (screen.firstChild) {
-        screen.removeChild(screen.firstChild);
+function clearCanvas() {
+    while (canvas.firstChild) {
+        canvas.removeChild(canvas.firstChild);
     }
 }
 
-const screen = document.querySelector(".screen");
-const controlScreenSize = document.querySelector(".control__screen-size");
-controlScreenSize.addEventListener("change", function(event) {
+function disableBrush(event) {
+    if (event.target.classList[0] !== "pixel") {
+        isDrawing = false;
+    }
+}
+
+function resizeCanvas(event) {
     let size = event.target.value;
-    createScreen(size)
-});
+    createCanvas(size);
+}
+
+const body = document.querySelector("body");
+const canvas = document.querySelector(".canvas");
+const controlCanvasSize = document.querySelector(".control__screen-size");
+
+body.addEventListener("mouseover", disableBrush);
+controlCanvasSize.addEventListener("change", resizeCanvas);
 
 // main()
-createScreen();
+createCanvas();
